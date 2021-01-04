e44e16f8b993261a974b352b167cbb45435a419b
Benjamin Li <benji112358@gmail.com>
2021-01-04 14:20:50 -0500

    Add super::Tick to AIController

--- Source/SimpleFPS/ShooterAIController.cpp
+++ Source/SimpleFPS/ShooterAIController.cpp
@@ -12,6 +12,7 @@ void AShooterAIController::BeginPlay(){
 }
 
 void AShooterAIController::Tick(float DeltaSeconds){
+    Super::Tick(DeltaSeconds);
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
     AAIController::MoveToActor(PlayerPawn);
 }
